0191def5a7b0f9528a2fb1f80131063cadb0bd6e
SuhasPopale <83824204+SuhasPopale@users.noreply.github.com>
2021-05-08 22:27:38 +0530

    Add files via upload

---
+++ Assignment2/PyramidPat2.java
@@ -0,0 +1,20 @@
+class PyramidPat2
+{
+	public static void main(String [] args)
+	{
+		for (int i=1; i<=9; i++)
+		{
+			int l=1;
+			for (int j=8; j>=i; j--)
+			{
+				System.out.print(" ");
+			}
+			for (int k=1; k<=i; k++)
+			{
+				System.out.print(l+" ");
+				l++;
+			}
+			System.out.println();
+		}
+	}
+}
